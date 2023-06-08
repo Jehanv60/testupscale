41bd733697f86bf1dc7ac6e99db45fd4276650ec
Jehanv60 <jehandwi@gmail.com>
2023-06-08 13:57:49 +0700

    Update Main.go

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"github/testupscale/soal1"
+	"github/testupscale/soal2"
+	"github/testupscale/soal3"
+	"github/testupscale/soal4"
 	"github/testupscale/soal5"
 )
 
 func main() {
-	// soal2.Soal2()
-	// a := soal4.Soal4(10)
-	// fmt.Println(a)
-	// soal3.Soal3()
-	// soal1.Soal1()
+	soal2.Soal2()
+	a := soal4.Soal4(10)
+	fmt.Println(a)
+	soal3.Soal3()
+	soal1.Soal1()
 	soal5.Soal5()
 }
